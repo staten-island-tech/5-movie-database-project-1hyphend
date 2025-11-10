af93f42a0342ff11c780e08f3efd93c11f5dfc3f
1hyphend <Ivanc221@nycstudents.net>
2025-11-10 10:16:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+
+# File 1
+# for index, item in enumerate(data):
+#         print(index, ":", item["title"])
+
+# File 2
+movie= input("give me some water speed ")
+while movie > 1968:
+    True
+    print ("title")
