84fa8dcf547962a170b1fb5d9fee4d372a94a13e
1hyphend <Ivanc221@nycstudents.net>
2025-11-12 10:11:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,77 @@ movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
 
 
-# File 1
+
+
+
+
+
+# File I
 # for index, item in enumerate(data):
 #         print(index, ":", item["title"])
 
-# File 2
-movie= input("give me some water speed ")
-while movie > 1968:
-    True
-    print ("title")
+# numero file II 
+    # year = int(input("Enter a year: "))
+for movie in movies:
+    if movie["year"] > movies["year"] :
+        print(f"{movie['title']} ({movie['year']})")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# numero file III 
+
+# start_year = int(input("Enter start year: "))
+# end_year = int(input("Enter end year: "))
+
+# print(f"\nMovies released between {start_year} and {end_year}:")
+# for movie in movies:
+#     if start_year < movie["year"] < end_year:
+#         print(f"{movie['title']} ({movie['year']})")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# numero files IV 
+
+# year = int(input("Enter a year: "))
+
+# print(f" (Movies released in {year}):")
+# for movie in movies:
+#     if movie["year"] == year:
+#         print(f"{movie['title']} ({movie['genre']})")
+
+
+
+
+
+
+
+
+
+
+# numebr files V 
+
+# search = input()
+# def search_movie(title):
+#     search = for m in movies if title.lower() in m["title"].lower()
+#     return search
